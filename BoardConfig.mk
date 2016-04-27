8a8a96282a87044eb159849e978697811ce092fa
Edoardo Zanoni <Ryther@users.noreply.github.com>
2016-04-27 12:47:20 +0200

    Update BoardConfig.mk: test for phasma kernel 2
    Corrected gcc version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ ENABLE_CPUSETS := true
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
-TARGET_GCC_VERSION_ARM64 := 4.9-kernel
+TARGET_GCC_VERSION_ARM64 := 7.0-kernel
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.0-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
